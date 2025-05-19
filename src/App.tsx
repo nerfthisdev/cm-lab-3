49eb923c2e10768810e2be04f2a25ea109bdc000
nerfthisdev <353054@niuitmo.ru>
2025-05-19 22:31:58 +0300

    feat: add app and main.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "katex/dist/katex.min.css";
+import IntegralCalculator from "./components/IntegralCalculator";
+
+function App() {
+  return (
+    <div className="bg-gray-800 min-h-screen p-6 text-white">
+      <IntegralCalculator />
+    </div>
+  );
+}
+
+export default App;
